c3d174858fcba957fa4e28c8ac6b4bf4fa5a057d
Mike Chambers <mistyn8@users.noreply.github.com>
2023-10-04 09:45:17 +0100

    Add `AutoRedirectLoginToExternalProvider` from appsetting configuration (#13)
    * Add AutoRedirectLoginToExternalProvider
    remove a couple of magicstrings
    correct the readme defaultgroup to editor
    optimisation Array.Empty<string>() over new string [] {}
    
    * remove empty statement
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Steve Temple <steve@gibedigital.com>

--- src/Umbraco.Community.AzureSSO/Settings/AzureSSOSettings.cs
+++ src/Umbraco.Community.AzureSSO/Settings/AzureSSOSettings.cs
@@ -15,7 +15,8 @@ namespace Umbraco.Community.AzureSSO.Settings
 		public string Icon => _configuration.Icon ?? "fa fa-lock";
 		public Dictionary<string, string> GroupLookup => _configuration.GroupBindings;
 		public bool SetGroupsOnLogin => _configuration.SetGroupsOnLogin ?? true;
-		public string[] DefaultGroups => _configuration.DefaultGroups ?? new string[] { };
+		public string[] DefaultGroups => _configuration.DefaultGroups ?? System.Array.Empty<string>();
 		public bool DenyLocalLogin => _configuration.DenyLocalLogin ?? false;
+		public bool AutoRedirectLoginToExternalProvider => _configuration.AutoRedirectLoginToExternalProvider ?? false;
 	}
 }
