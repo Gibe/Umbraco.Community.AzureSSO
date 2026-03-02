5e53a1ae1ad4d4859cfa20444b7935fccea3f4b2
Mike Chambers <mistyn8@users.noreply.github.com>
2026-03-02 09:43:06 +0000

    Add Net10 and Umbraco 17.0.0 targetting and the changed getStatus -> getStatusAsync HealthCheck method (#58)

--- src/Umbraco.Community.AzureSSO/HealthChecks/AzureSSOHealthCheck.cs
+++ src/Umbraco.Community.AzureSSO/HealthChecks/AzureSSOHealthCheck.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -6,7 +5,7 @@ using Umbraco.Cms.Core.HealthChecks;
 
 namespace Umbraco.Community.AzureSSO.HealthChecks
 {
-    [HealthCheck(HealthCheckId, HealthCheckName, Description = "Checks the Azure SSO config to ensure it is valid.", Group = "Configuration")]
+	[HealthCheck(HealthCheckId, HealthCheckName, Description = "Checks the Azure SSO config to ensure it is valid.", Group = "Configuration")]
 	public class AzureSSOHealthCheck : HealthCheck
 	{
 		private const string HealthCheckId = "07F7DA0A-D351-4347-92B3-9B607E1D38BB";
@@ -19,31 +18,39 @@ namespace Umbraco.Community.AzureSSO.HealthChecks
 			_configuration = configuration;
 		}
 
+#if NET10_0
+		public override async Task<IEnumerable<HealthCheckStatus>> GetStatusAsync()
+		{
+			return GetStatuses();
+		}
+#else
 		public override async Task<IEnumerable<HealthCheckStatus>> GetStatus()
 		{
-			var statuses = new List<HealthCheckStatus>();
+			return GetStatuses();
+		}
+#endif
 
+		private List<HealthCheckStatus> GetStatuses()
+		{
+			var statuses = new List<HealthCheckStatus>();
 			if (!_configuration.IsValid())
 			{
-				// TODO : We really need specific feedback for this to be useful
 				statuses.Add(new HealthCheckStatus("Configuration Invalid.")
 				{
 					Description = "Check AzureSSO configuration",
 					ResultType = StatusResultType.Error
 				});
 			}
-
-			if(!statuses.Any())
+			else
 			{
 				statuses.Add(new HealthCheckStatus("Configuration valid.")
 				{
 					ResultType = StatusResultType.Success
 				});
 			}
-
 			return statuses;
 		}
-		
+
 		public override HealthCheckStatus ExecuteAction(HealthCheckAction action) => new("How did you get here?")
 		{
 			ResultType = StatusResultType.Info
